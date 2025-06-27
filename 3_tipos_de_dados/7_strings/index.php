6e9ad516c4003057384e100d0dbe2a2baec7b9b1
luizvelasco <luizvelasco@gmail.com>
2025-06-27 08:48:09 -0300

    strings

---
+++ 3_tipos_de_dados/7_strings/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "Testando texto de aspas duplas <br>";
+    echo 'Testando texto de aspas simples <br>';
+    echo "ele disse: 'olá'<br>";
+    echo 'ele disse: "olá"<br>';
+
+    $idade = 15;
+
+    echo "Ele tem $idade anos<br>";
