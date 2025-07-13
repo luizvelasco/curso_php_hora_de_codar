08cdbd098381234bedc9624961efaaa5d60a439e
luizvelasco <luizvelasco@gmail.com>
2025-07-13 12:40:24 -0300

    exercicio8

---
+++ 4_variaveis/1_criacao_de_var/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $teste = "Algum texto";
+
+    echo $teste;
+
+    $num = 10;
+    echo "<br>";
+    echo $num;
+    echo "<br>";
+
+    $_nome = "Velasco";
+    echo $_nome;
+    echo "<br>";
+
+    $velocidadeMaxima = 100;
+    echo $velocidadeMaxima;
---
+++ 4_variaveis/2_exercicio_8/index.php
@@ -0,0 +1,7 @@
+<?php
+
+    $nome = "Joao";
+    $idade = 17;
+    $arr = [50, 60, 70];
+
+    echo $nome . " - " . $idade . " - " . $arr[2];
