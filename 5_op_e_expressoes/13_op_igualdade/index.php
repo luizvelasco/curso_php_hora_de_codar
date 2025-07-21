518b804ba0f2f28619ae67bf60e364a7f99c09b8
Luiz Velasco <luizvelasco@gmail.com>
2025-07-21 18:36:02 -0300

    exercicios

---
+++ 5_op_e_expressoes/13_op_igualdade/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    if(3 == 3) {
+        echo "Comparação verdadeira <br>";
+    }
+
+    if(3 == 4) {
+        echo "Comparação falsa <br>";
+    } 
---
+++ 5_op_e_expressoes/14_exercicio_15/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $a = 10;
+    $b = 10;
+
+    if ($a == $b) {
+        echo "a é igual a b<br>";
+    } else {
+        echo "a não é igual a b<br>";
+    }
