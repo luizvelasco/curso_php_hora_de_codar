73e7026dea0b7ba9d1c724324ee782be8ca29d28
luizvelasco <luizvelasco@gmail.com>
2025-08-09 17:18:07 -0300

    op ternario

---
+++ 5_op_e_expressoes/29_op_ternario/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    // operador ternário
+    // condição ? verdadeiro : falso;   
+
+    echo 20 > 10 ? "Deu true <br>" : "Deu false <br>";
+
+    echo 20 > 50 ? "Deu true <br>" : "Deu false <br>";
---
+++ 5_op_e_expressoes/30_exercicio_20/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    // Exercicio 20
+
+    $a = 10;
+    $b = 5; 
+
+    echo $a <= $b ? "Deu true" : "Deu false";
