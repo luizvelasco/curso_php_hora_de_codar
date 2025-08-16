d6439c41b14a8833afcd4f1227fe8b86b82eb119
luizvelasco <luizvelasco@gmail.com>
2025-08-16 16:21:54 -0300

    estrutura repeticao

---
+++ 07_estrutura_repeticao/01_while/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Definicao do contador
+    $x = 0;
+
+    while($x < 10){
+        echo $x . "<br>";
+        $x++;
+    }
+
+    $y = 10;
+
+    while($y > 0){
+        echo $y . "<br>";
+        $y--;
+    }
