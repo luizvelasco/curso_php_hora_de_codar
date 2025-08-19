65f88a0d9a97e246ba0feeadc4e5b9ee9313c8fb
Luiz Velasco <luizvelasco@gmail.com>
2025-08-19 19:24:41 -0300

    exer 30

---
+++ 07_estrutura_repeticao/08_do_while/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $j = 0;
+
+    do {
+        echo "Testando o while $j <br>";
+        $j++;
+
+    } while ($j <= 10);
---
+++ 07_estrutura_repeticao/11_exercicio_30b/index.php
@@ -0,0 +1,7 @@
+<?php
+    $arr = [];
+    for ($i=1; $i <= 10; $i++) { 
+        array_push($arr, $i);
+    }
+
+    print_r($arr);
